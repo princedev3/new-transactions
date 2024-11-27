a27d21b7c1a4188228886aad6ebac5d71420e936
princedev3 <marvinprince232@gmail.com>
2024-11-27 06:15:34 +0200

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@ import AddTransaction from "@/components/add-transaction";
 import DisplayUserBalance from "@/components/display-user-balance";
 import History from "@/components/history";
 import { Card, CardContent } from "@/components/ui/card";
+import { Suspense } from "react";
 export default function Home() {
   return (
-    <div className="grid place-items-center min-h-screen ">
-      <Card className="mx-auto grid w-full max-w-3xl p-4">
-        <CardContent>
-          <div className="grid  sm:grid-cols-2 content-start gap-5">
-            <div className="grid gap-y-3 content-start">
-              <DisplayUserBalance />
-              <AddTransaction />
+    <Suspense>
+      <div className="grid place-items-center min-h-screen ">
+        <Card className="mx-auto grid w-full max-w-3xl p-4">
+          <CardContent>
+            <div className="grid  sm:grid-cols-2 content-start gap-5">
+              <div className="grid gap-y-3 content-start">
+                <DisplayUserBalance />
+                <AddTransaction />
+              </div>
+              <div className="grid h-[90vh] overflow-y-scroll pr-5 custom-scroll-bar">
+                <History />
+              </div>
             </div>
-            <div className="grid h-[90vh] overflow-y-scroll pr-5 custom-scroll-bar">
-              <History />
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Suspense>
   );
 }
